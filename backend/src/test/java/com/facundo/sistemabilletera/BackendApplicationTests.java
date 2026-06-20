cafb7bc36596bfc159464cbb17c088c540582a27
Facundo <facundoaquiroga1@gmail.com>
2026-06-20 00:41:23 -0400

    first commit backend spring

---
+++ backend/src/test/java/com/facundo/sistemabilletera/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facundo.sistemabilletera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
